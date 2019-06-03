0376b79766c7088639c4a0d53866938d74a7324f
ChrsMark <chrismarkou92@gmail.com>
2019-06-03 07:49:50 +0000

    Add redis benchmark
    Signed-off-by: ChrsMark <chrismarkou92@gmail.com>

--- examples/main.go
+++ examples/main.go
@@ -3,24 +3,161 @@ package main
 import (
 	"fmt"
 	"time"
-
+	"strings"
+	"sync"
+	"bufio"
+	"os"
+	"log"
+        "net/http"
+	"encoding/json"
+	"math/rand"
 	"github.com/tylertreat/bench"
+	"github.com/tylertreat/hdrhistogram-writer"
 	"github.com/tylertreat/bench/requester"
 )
 
-func main() {
-	r := &requester.NATSRequesterFactory{
-		URL:         "nats://localhost:4222",
-		PayloadSize: 500,
-		Subject:     "benchmark",
-	}
+var wg sync.WaitGroup
+var slice []float64
+var queue = make(chan float64, 1)
 
-	benchmark := bench.NewBenchmark(r, 10000, 1, 30*time.Second, 0)
+
+
+func average(xs[]float64)float64 {
+	total:=0.0
+	for _,v:=range xs {
+		total +=v
+	}
+	return total/float64(len(xs))
+}
+
+
+
+func runBench(topic string) {
+	defer wg.Done()
+        r := &requester.RedisPubSubRequesterFactory{
+                URL:         ":6379",
+                PayloadSize:  10048,
+                Channel:     topic,
+        }
+	benchmark := bench.NewBenchmark(r, 20000, 1, 5*time.Second, 0)
 	summary, err := benchmark.Run()
 	if err != nil {
-		panic(err)
+	  panic(err)
 	}
+	//fmt.Println(summary.Throughput)
+	queue <- summary.Throughput
+}
 
-	fmt.Println(summary)
-	summary.GenerateLatencyDistribution(nil, "nats.txt")
+func runBenchWithRes(topic string) {
+	defer wg.Done()
+        r := &requester.RedisPubSubRequesterFactory{
+                URL:         ":6379",
+                PayloadSize:  10048,
+                Channel:     topic,
+        }
+	benchmark := bench.NewBenchmark(r, 20000, 1, 5*time.Second, 0)
+	summary, err := benchmark.Run()
+	if err != nil {
+	  panic(err)
+	}
+	//fmt.Println(summary.Throughput)
+	summary.GenerateLatencyDistribution(histwriter.Percentiles{50.0, 90.0, 95.0, 99.0, 99.9}, "redis.txt")
+	queue <- summary.Throughput
+}
+
+
+//RandomString - Generate a random string of A-Z chars with len = l
+func RandomString(len int) string {
+      bytes := make([]byte, len)
+      for i := 0; i < len; i++ {
+          bytes[i] = byte(65 + rand.Intn(25))  //A=65 and Z = 65+25
+      }
+      return string(bytes)
+}
+
+
+func main() {
+	fmt.Println("Starting benchmark")
+	go func() {
+	  for { 
+		fmt.Println("\n\nStarting new iteration")
+		wg.Add(301)
+		for i := 0; i < 300; i++ {
+			topic := RandomString(7)
+			//fmt.Println(topic)
+			go runBench(topic)
+		}
+		go runBenchWithRes("collector")
+		// Poll the queue for data and append it to the slice.
+		// Since this happens synchronously and in the same
+		// goroutine/thread, this can be considered safe.
+		go func() {
+			defer wg.Done()
+			for t := range queue {
+				slice = append(slice, t)
+			}
+		}()
+
+		wg.Wait()
+		//fmt.Println(slice)
+		fmt.Println("Average throughput:")
+		fmt.Println(average(slice))
+		f, _ := os.Open("redis.txt")
+		fs := bufio.NewScanner(f)
+		lateMap := make(map[string]string)
+		for fs.Scan() {
+			txt := fs.Text()
+			if strings.Contains(txt, "Value"){
+				continue
+			}
+			splitted := strings.Split(txt," ")
+			var slc []string 
+			for _, str := range splitted {
+				if str != ""{
+					slc = append(slc, str)
+				}
+			}
+			if len(slc) <= 1 {
+				continue
+			}
+			fmt.Println(txt)
+			lateMap[slc[1]] = slc[0]
+		}
+		fmt.Println(lateMap)
+		slcB, _ := json.Marshal(lateMap)
+		fmt.Println(string(slcB))
+	  }
+	}()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		f, _ := os.Open("redis.txt")
+		fs := bufio.NewScanner(f)
+		lateMap := make(map[string]string)
+		for fs.Scan() {
+			txt := fs.Text()
+			if strings.Contains(txt, "Value"){
+				continue
+			}
+			splitted := strings.Split(txt," ")
+			var slc []string 
+			for _, str := range splitted {
+				if str != ""{
+					slc = append(slc, str)
+				}
+			}
+			if len(slc) <= 1 {
+				continue
+			}
+			//fmt.Println(txt)
+			lateMap[slc[1]] = slc[0]
+		}
+		//fmt.Println(lateMap)
+		slcB, _ := json.Marshal(lateMap)
+		// fmt.Println(string(slcB))
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(slcB)
+	})
+
+	log.Fatal(http.ListenAndServe(":9080", nil))
+	//summary.GenerateLatencyDistribution(nil, "redis.txt")
 }
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/bench
+
+require (
+	github.com/Shopify/sarama v1.22.1
+	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd
+	github.com/garyburd/redigo v1.6.0
+	github.com/gocql/gocql v0.0.0-20190523124812-0680bfb96414
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/nats-io/go-nats v1.7.2
+	github.com/nats-io/go-nats-streaming v0.4.5
+	github.com/nats-io/nats.go v1.7.2 // indirect
+	github.com/nats-io/nkeys v0.0.2 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/nats-io/stan.go v0.4.5 // indirect
+	github.com/nsqio/go-nsq v1.0.7
+	github.com/streadway/amqp v0.0.0-20190404075320-75d898a42a94
+	github.com/tylertreat/bench v0.0.0-20170911233726-eb938b1b5e35
+	github.com/tylertreat/hdrhistogram-writer v0.0.0-20180430173243-73b8d31ba571
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
+	golang.org/x/tools v0.0.0-20190530043710-12d73424210d // indirect
+)
